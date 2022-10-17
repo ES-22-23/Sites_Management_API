c2b30102a586631c62a2991f6f6e10e3eed7174a
Vicente <vicente.costa@ua.pt>
2022-10-17 09:29:16 +0100

    fix codacy issues

--- src/main/java/es/module2/smapi/controller/SMAPIController.java
+++ src/main/java/es/module2/smapi/controller/SMAPIController.java
@@ -5,10 +5,9 @@ import es.module2.smapi.model.Owner;
 
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 @RestController
-class SMAPI_controller {
+class SMAPIController {
 
     @Autowired
     private OwnerRepository repository;
