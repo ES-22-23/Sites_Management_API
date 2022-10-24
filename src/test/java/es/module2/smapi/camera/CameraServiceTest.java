181d3fb50973f552135f24197f6c56f16d2c4ced
goncalo-machado <goncalofmachado01@gmail.com>
2022-10-24 09:50:00 +0100

    Fix unused imports

--- src/test/java/es/module2/smapi/camera/CameraServiceTest.java
+++ src/test/java/es/module2/smapi/camera/CameraServiceTest.java
@@ -1,37 +1,26 @@
 package es.module2.smapi.camera;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.IOException;
+import java.util.List;
 
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.google.gson.Gson;
 import org.junit.jupiter.api.AfterEach;
-
-import org.springframework.http.MediaType;
-import es.module2.smapi.model.Owner;
-import es.module2.smapi.model.Property;
-import es.module2.smapi.model.Camera;
-import es.module2.smapi.service.SMAPIService;
-import es.module2.smapi.SmapiApplication;
-import es.module2.smapi.repository.CameraRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import java.util.List;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.is;
+import com.google.gson.Gson;
+
+import es.module2.smapi.SmapiApplication;
+import es.module2.smapi.model.Camera;
+import es.module2.smapi.model.Owner;
+import es.module2.smapi.model.Property;
+import es.module2.smapi.repository.CameraRepository;
+import es.module2.smapi.service.SMAPIService;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK, classes = SmapiApplication.class)
