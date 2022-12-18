d29f5d75cfe35d67764b79ac55dcb6e7377254a9
goncalo-machado <goncalofmachado01@gmail.com>
2022-12-18 22:59:55 +0000

    Fix healthStatus

--- src/main/java/es/module2/smapi/entities/HealthStatus.java
+++ src/main/java/es/module2/smapi/entities/HealthStatus.java
@@ -3,9 +3,12 @@ package es.module2.smapi.entities;
 import lombok.Data;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 public class HealthStatus {
 
+    @JsonProperty("isHealthy")
     private final boolean isHealthy;
     private final List<?> additionalProperties;
 
