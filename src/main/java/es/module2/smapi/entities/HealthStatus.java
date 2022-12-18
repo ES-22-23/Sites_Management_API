3d8c24185bffd3f6f49d674387d25481488a76a5
goncalo-machado <66827806+goncalo-machado@users.noreply.github.com>
2022-12-18 22:38:21 +0000

    Update HealthStatus.java

--- src/main/java/es/module2/smapi/entities/HealthStatus.java
+++ src/main/java/es/module2/smapi/entities/HealthStatus.java
@@ -1,11 +1,12 @@
 package es.module2.smapi.entities;
 
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class HealthStatus {
 
-    private final boolean isAvailable;
-    private final boolean databaseAvailable;
+    private final boolean isHealthy;
+    private final List<?> additionalProperties;
 
 }
