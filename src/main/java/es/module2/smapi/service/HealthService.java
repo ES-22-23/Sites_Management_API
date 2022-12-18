72bc84313ac3a77b98717dbd244bf4ae85ff399f
goncalo-machado <66827806+goncalo-machado@users.noreply.github.com>
2022-12-18 22:39:37 +0000

    Update HealthService.java

--- src/main/java/es/module2/smapi/service/HealthService.java
+++ src/main/java/es/module2/smapi/service/HealthService.java
@@ -2,12 +2,13 @@ package es.module2.smapi.service;
 
 import es.module2.smapi.entities.HealthStatus;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 
 @Service
 public class HealthService {
 
     public HealthStatus getHealthStatus() {
-        return new HealthStatus(true, true);
+        return new HealthStatus(true, new ArrayList<>());
     }
 
 }
